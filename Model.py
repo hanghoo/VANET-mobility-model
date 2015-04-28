66385010a43673313a146582cb1973679e1efebb
Miguel Uzcategui <muzcategui1106@gmail.com>
2015-04-28 17:28:44 -0400

    Addition of cars into the model

--- Model.py
+++ Model.py
@@ -2,10 +2,25 @@ import matplotlib.pyplot as pl
 from pylab import *
 import numpy as np
 import random
+from Car import Car
 
+#constants
+LANES = 4
+DS =10 #meters
+TOLERANCE_TIME=0 #second
+MIN_SPEED = 50 #meters/s
+MAX_SPEED = 70 #meters/s
+TRAFFIC_VOLUME = 3000 #vehicles/hour/lane
+ARR_DEP_RATE = 0.833 # arrival/ departure rate
+TRAFFIC_DENSITY_MIN = 100 # minimum traffic density per lane
+TRAFFIC_DENSITY_MAX = 500 # maximum traffic density per lane
 figure(0)
 pl.ylim([0,5000])   # setting the grid length to 5 units (assume 1 unit = 1mile)
 pl.xlim([0,12])  # Generate 4 lanes with distance between each as 3 units
+
+#variables
+model_cars = []
+lanes_cars_num = [0,0,0,0]
 
 
 '''########################################################################
@@ -58,13 +73,46 @@ def plot_grid(entry_ramp_positions):
 
     #pl.arrow(6,entry_ramp_positions, 0.5, 0.5, hold=None)
     #pl.annotate("", xy=(0.5, 0.5), xytext=(),arrowprops=dict(arrowstyle="->"))
-    pl.show()
 
+
+"""####################################################
+Function name: start_model
+Description: start the model by distributing 100 cars
+             per lane randomly but uniformly
+Input Parameters: the initial plot of the model
+Output parameters: a list containing a list of all the vehicle positions
+#######################################################"""
+def start_model():
+    model_positions =[]
+    for i in range(LANES):
+        positions = []
+        lane = 2 +(i*3) #lane position, the + 4 is the offset from the y axis
+        for j in range(TRAFFIC_DENSITY_MIN):
+            # generate the car position
+            while True:
+                y_axis = np.random.uniform(0,5000)
+
+                #make sure psoition of two cars dont overlap
+                if  not positions.__contains__(y_axis):
+                    positions.append(y_axis)
+                    break
+            car = Car(lane,y_axis)
+            model_cars.append(car)
+            model_positions.append(car.get_coordinates())
+            pl.scatter(lane,y_axis,1)
+
+            #add car to the lane
+            lanes_cars_num[i]+=1
 
 
 entry_ramp_positions = generate_entry_ramp_positions()
 print(entry_ramp_positions)
 plot_grid(entry_ramp_positions)
+start_model()
+pl.show()
+
+
+
 
 
 
